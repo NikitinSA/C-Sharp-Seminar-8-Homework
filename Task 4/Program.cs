2caeb98a37f5a2fce4b99ad8dd4b125ad111629b
Савелий <Savka9765@yandex.ru>
2022-09-03 18:17:21 +0300

    Трехмерная матрица

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -56,10 +56,9 @@ void PrintArray(int[,,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write( $"({i},{j}");
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                Console.Write( $",{k}) ({i},{j},{k}) {array[i,j,k]} ");
+                Console.Write( $"{array[i,j,k]} ({i},{j},{k}) ");
             }
             Console.WriteLine();
         }
